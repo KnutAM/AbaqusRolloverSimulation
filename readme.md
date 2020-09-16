0131ec2a67732f1415aaa2a6652810623562ecec
Knut Andreas Meyer <knutam@gmail.com>
2020-09-16 15:50:28 +0200

    Updated readme and user_settings_example to reflect new method for adding user material routines

--- readme.md
+++ readme.md
@@ -16,6 +16,14 @@ Script to setup rollover simulation in Abaqus for CHARMEC
 1. Create a new file in the `src/` directory named `user_settings.py` and copy the contents from `user_settings_example.py`
 2. Run the python script `src/wheel_super_element/create_super_wheel.py` from a directory in which you wish to save the wheel super element
 3. This will create files `uel.for`, `uel_coords.npy` and `uel_info.json` in the folder from which you called `create_super_wheel.py`
-4. In the same folder, call `abaqus make library=uel` (If linux, rename `uel.for` to `uel.f`)
-5. Edit `user_settings.py` to update `super_element_path` to the folder containing the `uel.for` that you created previously. 
+5. Edit `user_settings.py` to update `super_element_path` to the folder containing the `uel.for`/`uel.f` that you created previously. 
 6. Run `src/rollover_model.py` from Abaqus
+
+## Using material subroutines
+
+To use a material subroutine see the material `chaboche_umat` in `user_settings_example.py` for inspiration. The field `'material_model'` must be set to  `'user'` and the field `'mpar'` must contain a dictionary with the following fields:
+
+* `'nstatv'`: Give an int for how many state variables the material model requires
+* `'src_folder'`: Give the path to the folder containing a file named `umat.for`/`umat.f`. The folder must contain all required files such that calling `abaqus make library=umat.for` (or `umat.f` ) would work in that folder (This will not be done however, the content will be copied)
+* `'user_mpar_array'`: A tuple containing the material parameter that should be supplied to the user material routine
+
--- src/user_settings_example.py
+++ src/user_settings_example.py
@@ -8,33 +8,19 @@ numtrick = {'dummy_stiffness': 1.e-6,   # MPa   (Stiffness for dummy materials e
             'move_back_time': 1.e-6,
            }
 
+# Base folders to shorten paths later in this file
+super_element_base_folder = ('C:/Users/knutan/Documents/Work/ProjectsWorkFolders/MU34/' + 
+                             'Project_2020_C_RolloverSimulation/super_wheels/')
+material_model_folder = 'C:/Users/knutan/Documents/Work/MySoftwares/MaterialModels/compiled_abaqus/'
+
 # Rail settings
 rail_geometry = {'length': 30.0, 'height': 30.0, 
                  'max_contact_length': max_contact_length}
 rail_mesh = {'fine': 2.0, 'coarse': 5.0}
 
-super_element_path = ('C:/Users/knutan/Documents/Work/ProjectsWorkFolders/MU34/' + 
-                     'Project_2020_C_RolloverSimulation/super_wheels/' +
-                     'D400_M01p000')
+# Wheel settings
+super_element_path = (super_element_base_folder + 'D400_M04p000')
 
-# Material settings
-# Only dictionary "materials" required, the remaining are only support variables
-elastic_steel = {'material_model': 'elastic', 'mpar': {'E': 210.e3, 'nu': 0.3}}
-plastic_steel = {'material_model': 'chaboche', 'mpar': {'E': 210.e3, 'nu': 0.3, 'Y0': 200., 
-                                                        'Hkin': 5.e3, 'binf': 500., 'Hiso': 1.e3, 
-                                                        'kinf': 100.}
-                }
-chaboche_umat = {'material_model': 'user', 
-                 'mpar': {'nstatv': 8, 'umat': 'chaboche.obj', 
-                 'user_mpar_array': (210.e3, 0.3, 500., 1.e3, 1.e-2, 5.e3, 2.e-4)}
-                }
-dummy_elastic = {'material_model': 'elastic', 'mpar': {'E': numtrick['dummy_stiffness'], 'nu': 0.3}
-                }
-
-materials = {'rail': elastic_steel, 'wheel': elastic_steel, 
-             'shadow_rail': dummy_elastic, 
-             'contact_trusses_wheel': dummy_elastic}
-             
 # Time increment settings
 time_incr_param = {'nom_num_incr_rolling': 50,      # Nominal number of increments during rolling
                    'max_num_incr_rolling': 200,     # Maximum number of increments during rolling
@@ -51,4 +37,25 @@ load_parameters = {'initial_depression': 0.01,      # mm
 contact_parameters = {'penalty_stiffness': 1.e6,    # N/mm
                       'friction': 0.2,              # -
                       }
-                      
+
+# Material settings
+# Only dictionary "materials" required, the remaining are only support variables
+elastic_steel = {'material_model': 'elastic', 'mpar': {'E': 210.e3, 'nu': 0.3}}
+plastic_steel = {'material_model': 'chaboche', 'mpar': {'E': 210.e3, 'nu': 0.3, 'Y0': 200., 
+                                                        'Hkin': 5.e3, 'binf': 500., 'Hiso': 1.e3, 
+                                                        'kinf': 100.}
+                }
+
+chaboche_umat = {'material_model': 'user', 
+                 'mpar': {'nstatv': 8, 
+                          'src_folder': material_model_folder + 'chaboche',
+                          'user_mpar_array': (210.e3, 0.3, 500., 1.e3, 1.e-2, 5.e3, 2.e-4)
+                          }
+                }
+                
+dummy_elastic = {'material_model': 'elastic', 'mpar': {'E': numtrick['dummy_stiffness'], 'nu': 0.3}
+                }
+
+materials = {'rail': chaboche_umat, 'wheel': elastic_steel, 
+             'shadow_rail': dummy_elastic, 
+             'contact_trusses_wheel': dummy_elastic}
