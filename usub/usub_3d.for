9b86a4e86cc7d3d51d6f55d9ece8c332b57553ee
Knut Andreas Meyer <knutam@gmail.com>
2020-11-06 15:47:14 +0100

    Added special includes file, to avoid duplicate code

--- usub/usub_3d.for
+++ usub/usub_3d.for
@@ -2,22 +2,7 @@
 ! Abaqus utility modules
 !include 'abaqus_utils_mod.f90'          ! Do not include when running Abaqus
 !include 'abaqus_utils_dummy_mod.f90'    ! Include when running Abaqus
-! fortran-utilities modules
-include 'utils/src/resize_array_mod.f90'
-include 'utils/src/sort_mod.f90'
-include 'utils/src/find_mod.f90'
-include 'utils/src/linalg_mod.f90'
-! Rollover modules
-include 'usub_utils_mod.f90'
-include 'filenames_mod.f90'
-include 'step_type_mod.f90'
-include 'uel_stiff_mod.f90'
-include 'uel_trans_mod.f90'
-include 'node_id_mod.f90'
-include 'bc_mod.f90'
-include 'load_param_mod.f90'
-include 'disp_mod.f90'
-include 'urdfil_mod.f90'
+include 'includes.f90'
 
 
 ! ============================================== UEL ==============================================
